e04886b92526d38905e1409736743e6f3940be91
Felician Nemeth <nemethf@tmit.bme.hu>
2012-08-06 07:03:22 -0700

    remove .dir-locals.el

--- butterfly_app/.dir-locals.el
+++
@@ -1,8 +0,0 @@
-((c++-mode . ((cc-search-directories "."
-				     "/usr/include"
-				     "/usr/local/include/*"
-				     "../../../include")
-	      (ffap-c-path "/usr/include"
-			   "/usr/local/include"
-			   "../../../include")
-	      (indent-tabs-mode . nil))))
